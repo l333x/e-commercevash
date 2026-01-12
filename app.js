9bfe8b81da3c763b50f1e13f74ccf0bfe1de1fd9
Victor Segura <vashalex70@gmail.com>
2026-01-12 16:53:36 -0500

    Agregar EJS al proyecto y creacion de la carpeta

--- app.js
+++ app.js
@@ -6,10 +6,34 @@ const app = express();
 
 // 3. DEfinir un puesrto sobre el cual funcione la aplicacion
 const PORT = 3000;
+// ----configuracion----
+//EStablecer ejs como motor de plantillas
+app.set('view engine', 'ejs');
 
-//4. CRear la primera "RUta" cuando el usuario ingrese a la raiz de la aplicacion
+//EStablecer la carpeta publica con los elementos estaticos (css, imagenes, js cliente)
+app.use(express.static('public'));
+
+
+//4. crear la primera "ruta" cuando el usuario ingrese a la raiz de nuestro sitio y un boton que me redirija a /productos
 app.get('/', (req, res) => {
-    res.send('<h1>Servidor de E-commerce Iniciando</h1><p>NPM Y EXPRESS FUNCIONANDO...</p>');
+    res.send(`
+        <h1>Bienvenido a mi tienda en línea</h1>
+        <p>Explora nuestros productos exclusivos.</p>
+        <a href="/productos"><button>Ver Productos</button></a>
+    `);
+});
+
+//ruta para /productos
+app.get('/productos', (req, res) => {
+    res.send(`
+        <h1>Lista de Productos</h1>
+        <ul>
+            <li>Producto 1</li>
+            <li>Producto 2</li>
+            <li>Producto 3</li>
+        </ul>
+        <a href="/"><button>Volver al Inicio</button></a>
+    `);
 });
 // 5. Encender el servidor
 app.listen(PORT, () => {
